3cd9a569686997a661ea83836c755c37f86e2f98
Om Narkhede <developers.omn@gmail.com>
2025-04-02 22:20:26 +0530

    logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -21,9 +21,17 @@ const Navigation = () => {
       <div className="container mx-auto px-4 md:px-6 py-4">
         <div className="flex items-center justify-between">
           {/* Left side - Logo */}
-          <Link to="/" className="flex items-center">
-            <h1 className="text-xl sm:text-2xl font-display font-bold gradient-text">QUIZZERS</h1>
-          </Link>
+          <div className="flex items-center space-x-4">
+            <Link to="/" className="flex items-center">
+              <h1 className="text-xl sm:text-2xl font-display font-bold gradient-text">QUIZZERS</h1>
+            </Link>
+            {/* Mobile College Logo */}
+            <img 
+              src="https://bvcoe.bharatividyapeeth.edu/media/images/bvcoe_logo_3.png" 
+              alt="BVCOE Logo" 
+              className="h-8 w-auto object-contain md:hidden"
+            />
+          </div>
 
           {/* Middle - Navigation Links and College Logo */}
           <div className="hidden md:flex items-center justify-center flex-1">
@@ -57,11 +65,6 @@ const Navigation = () => {
         {/* Mobile Navigation Menu */}
         {isMenuOpen && (
           <div className="md:hidden py-4 flex flex-col items-center space-y-4">
-            <img 
-              src="https://bvcoe.bharatividyapeeth.edu/media/images/bvcoe_logo_3.png" 
-              alt="BVCOE Logo" 
-              className="h-12 w-auto object-contain mb-4"
-            />
             {navItems.map((item) => (
               <a
                 key={item.name}
